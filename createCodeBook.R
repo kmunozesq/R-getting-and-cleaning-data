3dbba8630faa21b4aaab78badba69b86c05aa4ae
kmunozesq <k.munozesquivel@gmail.com>
2015-01-25 19:36:12 +0000

    Removing a file that had not to be added

--- createCodeBook.R
+++
@@ -1,7 +0,0 @@
-createCodeBook <- function()
-{
-        dir <- "data_cln_project"
-        DT <- read.table(file=file.path(getwd(),dir,"tidy_data.txt"), header=TRUE, sep ="") %>%
-                str
-                #writeLines(file.path(getwd(),"CodeBook.txt"), sep ="/t")
-}
